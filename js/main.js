cdb9c3f13cf5f11ab90bd8bd50b2afc7dcf876f3
ArturSergeevi4 <arturshvaiberov@rocketmail.com>
2020-04-19 19:24:04 +0600

    Update main.js
    Deleted fixed menu script

--- js/main.js
+++ js/main.js
@@ -9,23 +9,4 @@ function toggleModal() {
 	modal.classList.toggle('is-open')
 }
 
-$(function() {
-  menu_top = $('.header').offset().top;        // запоминаем положение меню
-  $(window).scroll(function () {             // отслеживаем событие прокрутки страницы
-    if ($(window).scrollTop() > menu_top) {  // если прокрутка дошла до меню
-      if ($('.header').css('position') != 'fixed') {  // проверяем, если меню еще не зафиксировано
-        $('.header').css('position','fixed');  // задаем блоку меню свойство position = fixed
-        $('.header').css('top','0');           // положение в самом верху
-        $('.main').css('margin-top','80px'); // делаем отступ, чтобы контент не "скакал" в момент фиксации меню
-      }
-    } else {                                 // прокрутка страницы обратно вверх достигла место "перехода" меню
-      if ($('.header').css('position') == 'fixed') {  // если меню зафиксировано
-        $('.header').css('position','');
-        $('.header').css('top','');
-        $('.main').css('margin-top','');
-      }
-    }
-  });
-});
-
 new WOW().init()
