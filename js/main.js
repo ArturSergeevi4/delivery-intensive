c722e82832620ecc0360625918df48410c912fcb
Artur Shvaiberov <3.13.13@mail.ru>
2020-04-26 01:03:47 +0600

    Added dropdown, added telegram aut and more...

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
 const cartButton = document.querySelector('#cart-button')
 const modal = document.querySelector('.modal')
 const closeButton = document.querySelector('.close')
-
 cartButton.addEventListener('click', toggleModal)
 closeButton.addEventListener('click', toggleModal)
-
 function toggleModal() {
 	modal.classList.toggle('is-open')
+}
+function closeModal() {
+	modal.classList.remove('is-open')
+}
+
+function openModal() {
+	modal.classList.add('is-open')
+}
+
+let searchInput = document.querySelector('.input-from')
+let additionalInput = document.querySelector('.search-wrapper-additional')
+searchInput.onfocus = inputFocus
+function inputFocus() {
+	additionalInput.classList.add('search-wrapper-additional-visible')
 }
 
 new WOW().init()
